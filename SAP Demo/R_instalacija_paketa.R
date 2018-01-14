d5cf1ab742f7b6f4d6989890aa2e6fa5d40c9b5e
Karlo Grlić <karlogrlicweb@gmail.com>
2018-01-14 15:12:39 +0100

    SAP Demo

---
+++ SAP Demo/R_instalacija_paketa.R
@@ -0,0 +1,54 @@
+﻿#
+#
+#   skripta za instaliranje paketa koji su potrebni za vježbe
+#
+#   Otvoriti ovu skriptu u RStudiu, označiti mišem cijeli tekst i pokrenuti klikom na Run
+#
+
+#
+#	update paketa koji dolaze uz instalaciju
+#
+
+update.packages(ask='graphics',checkBuilt=TRUE)
+
+#
+#   paketi za vježbe
+#
+paketi.vjezbe<-c(
+	"effects",
+	"ggplot2",
+	"gplots",
+	"qgraph",
+	"HH",
+	"Hmisc",
+	"lattice",
+	"latticeExtra",
+	"latticeDensity",
+	"latticist",
+	"learningr",
+	"samplesize",
+	"sampling",
+	"multcomp",
+	"multcompView",
+	"OIdata",
+	"openintro",
+	"png",
+	"pwr",
+	"powerAnalysis",
+	"RODBC",
+	"vcd",
+	"XLConnect",
+	"knitr",
+	"markdown",
+	"rmarkdown")
+#
+#   instalacija
+#
+
+install.packages(pkgs=paketi.vjezbe, dependencies=T)
+
+#
+#	 čišćenje
+#
+
+rm(paketi.vjezbe)
